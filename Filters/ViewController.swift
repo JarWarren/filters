b3acdc996c35b5165bd5c2d0e87b1668b69f83f0
JarWarren <gentlemanjaruna@gmail.com>
2020-04-28 17:22:26 -0600

    add vibrance filter

--- Filters/ViewController.swift
+++ Filters/ViewController.swift
@@ -12,7 +12,7 @@ import CoreImage
 class ViewController: UIViewController {
     
     // MARK: - Properties
-
+    
     var context = CIContext()
     var filterOne = CIFilter(name: "CITemperatureAndTint") // inputNeutral, inputTargetNeutral
     var filterTwo = CIFilter(name: "CIVibrance") // amount
@@ -38,24 +38,41 @@ class ViewController: UIViewController {
         // temperature and tint
         filterOne?.setValue(startingImage, forKey: kCIInputImageKey)
         
+        // vibrance
         filterTwo?.setValue(startingImage, forKey: kCIInputImageKey)
+        filterTwo?.setValue(0, forKey: kCIInputAmountKey)
         filterThree?.setValue(startingImage, forKey: kCIInputImageKey)
         filterFour?.setValue(startingImage, forKey: kCIInputImageKey)
     }
-
+    
+    /**
+     A "neutral" value is around 6,500. Lower values like 2-3,000 are very orange or "hot". Higher values like 12-14,000 are blue or "cold".
+     Orange values are much denser than blue values (sliding left turns orange faster).
+     Current slider range is 3,500 - 14,000 and starts at 6,500.
+     
+     CITemperatureAndTint takes inputNeutral && inputTargetNeutral, two different CIVectors.
+     inputNeutral: Starts at [6,500, 0] by default and I'm leaving it.
+     inputTargetNeutral: Same as above but I'm letting the user alter the X value.
+     */
     private func processTemperature(_ value: Float) {
         let temperature = CIVector(x: CGFloat(value))
         filterOne?.setValue(temperature, forKey: "inputTargetNeutral")
-        guard let outputImage = filterOne?.outputImage,
-            let processedImage = context.createCGImage(outputImage, from: outputImage.extent) else { return }
-            filterImageView.image = UIImage(cgImage: processedImage)
+        guard let output = filterOne?.outputImage,
+            let processedImage = context.createCGImage(output, from: output.extent) else { return }
+        filterImageView.image = UIImage(cgImage: processedImage)
     }
     
-    private func processVibrance() {
-        
+    /**
+     Vibrance is measured from -1 - 1.
+     */
+    private func processVibrance(_ value: Float) {
+        filterTwo?.setValue(value, forKey: kCIInputAmountKey)
+        guard let output = filterTwo?.outputImage,
+            let processed = context.createCGImage(output, from: output.extent) else { return }
+        filterImageView.image = UIImage(cgImage: processed)
     }
     
-    private func processHue() {
+    private func processHue(_ value: Float) {
         
     }
     
@@ -66,34 +83,34 @@ class ViewController: UIViewController {
     }
     
     @IBAction func sliderTwoValueChanged(_ sender: UISlider) { // vibrance
-        processVibrance()
+        processVibrance(sender.value)
     }
     
     @IBAction func sliderThreeValueChanged(_ sender: UISlider) { // hue
-        processHue()
+        processHue(sender.value)
     }
 }
 
 /*
  @IBAction func blurSlider(_ sender: UISlider) {
-     let originalImage = UIImage(named: "yourimage")// replace yourimage with the name of your image
-     let currentValue = Int(sender.value)
-
-     blur.maximumValue = 5
-     blur.minimumValue = 0
-
-     let currentFilter = CIFilter(name: "CIBoxBlur")
-     currentFilter!.setValue(CIImage(image: originalImage!), forKey: kCIInputImageKey)
-     currentFilter!.setValue(currentValue, forKey: kCIInputRadiusKey)
-
-     let cropFilter = CIFilter(name: "CICrop")
-     cropFilter!.setValue(currentFilter!.outputImage, forKey: kCIInputImageKey)
-     cropFilter!.setValue(CIVector(cgRect: (CIImage(image: originalImage!)?.extent)!), forKey: "inputRectangle")
-
-     let output = cropFilter!.outputImage
-     let cgimg = context.createCGImage(output!, from: output!.extent)
-     let processedImage = UIImage(cgImage: cgimg!)
-     imageView.image = processedImage
-
+ let originalImage = UIImage(named: "yourimage")// replace yourimage with the name of your image
+ let currentValue = Int(sender.value)
+ 
+ blur.maximumValue = 5
+ blur.minimumValue = 0
+ 
+ let currentFilter = CIFilter(name: "CIBoxBlur")
+ currentFilter!.setValue(CIImage(image: originalImage!), forKey: kCIInputImageKey)
+ currentFilter!.setValue(currentValue, forKey: kCIInputRadiusKey)
+ 
+ let cropFilter = CIFilter(name: "CICrop")
+ cropFilter!.setValue(currentFilter!.outputImage, forKey: kCIInputImageKey)
+ cropFilter!.setValue(CIVector(cgRect: (CIImage(image: originalImage!)?.extent)!), forKey: "inputRectangle")
+ 
+ let output = cropFilter!.outputImage
+ let cgimg = context.createCGImage(output!, from: output!.extent)
+ let processedImage = UIImage(cgImage: cgimg!)
+ imageView.image = processedImage
+ 
  }
  */
