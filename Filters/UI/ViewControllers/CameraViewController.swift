0d598ceac7a0243b4dc7ca975b256ebc5547a3e8
JarWarren <gentlemanjaruna@gmail.com>
2020-04-30 17:46:09 -0600

    update cameraviewcontroller

--- Filters/UI/ViewControllers/CameraViewController.swift
+++ Filters/UI/ViewControllers/CameraViewController.swift
@@ -12,12 +12,16 @@ import Metal
 class CameraViewController: UIViewController {
     
     @IBOutlet weak var filterImageView: FilterImageView!
+    @IBOutlet weak var temperatureSlider: UISlider!
+    @IBOutlet weak var vibranceSlider: UISlider!
+    @IBOutlet weak var hueSlider: UISlider!
+    @IBOutlet weak var sharpnessSlider: UISlider!
     
     let photoPicker = PhotoPicker()
     let chain = ChainFilter(filters: .temperature,
-                                .vibrance,
-                                .hue,
-                                .sharpness)
+                            .vibrance,
+                            .hue,
+                            .sharpness)
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -26,6 +30,14 @@ class CameraViewController: UIViewController {
         photoPicker.delegate = self
         chain.setImage(#imageLiteral(resourceName: "sample0"))
         chain.delegate = self
+    }
+    
+    @IBAction func undoButtonTapped(_ sender: Any) {
+        chain.resetFilters()
+        temperatureSlider.value = 6500
+        vibranceSlider.value = 0
+        hueSlider.value = 0
+        sharpnessSlider.value = 0.4
     }
     
     @IBAction func cameraButtonTapped(_ sender: Any) {
@@ -79,75 +91,3 @@ extension CameraViewController: PhotoPickerDelegate {
         }
     }
 }
-
-/*
- class EditingViewController: UIViewController, MTKViewDelegate {
-     @IBOutlet weak var slider: UISlider!
-     @IBOutlet weak var mtkview: MTKView!
-
-     var context : CIContext!
-     let displayImage : CIImage! // must be set before viewDidLoad
-     let vig = VignetteFilter()
-     var queue: MTLCommandQueue!
-
-     // slider value changed
-     @IBAction func doSlider(_ sender: Any?) {
-         self.mtkview.setNeedsDisplay()
-     }
-
-     override func viewDidLoad() {
-         super.viewDidLoad()
-
-         // preparation, all pure boilerplate
-
-         self.mtkview.isOpaque = false // otherwise background is black
-         // must have a "device"
-         guard let device = MTLCreateSystemDefaultDevice() else {
-             return
-         }
-         self.mtkview.device = device
-
-         // mode: draw on demand
-         self.mtkview.isPaused = true
-         self.mtkview.enableSetNeedsDisplay = true
-
-         self.context = CIContext(mtlDevice: device)
-         self.queue = device.makeCommandQueue()
-
-         self.mtkview.delegate = self
-         self.mtkview.setNeedsDisplay()
-     }
-
-     func mtkView(_ view: MTKView, drawableSizeWillChange size: CGSize) {
-     }
-
-     func draw(in view: MTKView) {
-         // run the displayImage thru the CIFilter
-         self.vig.setValue(self.displayImage, forKey: "inputImage")
-         let val = Double(self.slider.value)
-         self.vig.setValue(val, forKey:"inputPercentage")
-         var output = self.vig.outputImage!
-
-         // okay, `output` is the CIImage we want to display
-         // scale it down to aspect-fit inside the MTKView
-         var r = view.bounds
-         r.size = view.drawableSize
-         r = AVMakeRect(aspectRatio: output.extent.size, insideRect: r)
-         output = output.transformed(by: CGAffineTransform(
-             scaleX: r.size.width/output.extent.size.width,
-             y: r.size.height/output.extent.size.height))
-         let x = -r.origin.x
-         let y = -r.origin.y
-
-         // minimal dance required in order to draw: render, present, commit
-         let buffer = self.queue.makeCommandBuffer()!
-         self.context!.render(output,
-             to: view.currentDrawable!.texture,
-             commandBuffer: buffer,
-             bounds: CGRect(origin:CGPoint(x:x, y:y), size:view.drawableSize),
-             colorSpace: CGColorSpaceCreateDeviceRGB())
-         buffer.present(view.currentDrawable!)
-         buffer.commit()
-     }
- }
- */
